92e08186d2ea3431832903f2cd72dd8f4fe0ec2f
Djulooo <jules.bellegante@my-digital-school.org>
2024-01-13 20:40:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// App.js
+
+import React from 'react';
+import './App.css';
+import Footer from './footer';
+
+const HomePage = () => {
+  return (
+    <div className="app">
+      <header>
+        <div className="banner-container">
+        </div>
+      </header>
+      <div className="titre">
+      </div>
+
+      <div className="square-container">
+          <div className="square1">
+          </div>
+          <div className="square2">
+          </div>
+        </div>
+      <Footer/>
+    </div>
+  );
+};
+
+
+export default HomePage;
